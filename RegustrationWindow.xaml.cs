ae69ee8878d8697cbab1913b27cbf3471634bd8c
Orionpax <emil2008abdullaev@gmail.com>
2026-06-08 14:18:56 +0300

    Add files via upload

--- RegustrationWindow.xaml.cs
+++ RegustrationWindow.xaml.cs
@@ -19,31 +19,56 @@ namespace RealEstateAgency
     /// </summary>
     public partial class RegustrationWindow : Window
     {
+        public static Dictionary<string, List<string>> UsersDatabase = new Dictionary<string, List<string>>();
         public RegustrationWindow()
         {
             InitializeComponent();
+            txtBlkPassword.IsEnabled = false;
+            txtBlkPassword2.IsEnabled = false;
         }
-
         private void btnRegistration_Click(object sender, RoutedEventArgs e)
         {
-            if (String.IsNullOrWhiteSpace(txtBlkFullname.Text)) 
+            string fullname = txtBlkFullname.Text.Trim();
+            string email = txtBxEmail.Text.Trim();
+            string pass1 = txtBxPassword.Password;
+            string pass2 = txtBxPassword2.Password;
+            if (String.IsNullOrWhiteSpace(fullname))
             {
-                MessageBox.Show("Поле ФИО не заполенено!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Поле ФИО не заполнено!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return; 
             }
-            if (txtBxPassword.Password == txtBxPassword2.Password)
+            if (String.IsNullOrWhiteSpace(email))
             {
-                MessageBox.Show("Вы зарегистрированны! Нажмите кнопку 'Зарегистрироваться'.", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Поле email не заполнено!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
             }
-            if (String.IsNullOrWhiteSpace(txtBxPassword.Password) && String.IsNullOrWhiteSpace(txtBxPassword2.Password)) 
+            if (String.IsNullOrWhiteSpace(pass1) || String.IsNullOrWhiteSpace(pass2))
             {
-
+                MessageBox.Show("Поле пароль или подтверждение пароля не заполнено!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
             }
-            else
+            if (UsersDatabase.ContainsKey(email))
+            {
+                MessageBox.Show("Пользователь с таким Email уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (pass1 != pass2)
             {
                 MessageBox.Show("Пароли не совпадают!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 txtBxPassword.Clear();
                 txtBxPassword2.Clear();
+                return;
             }
+            List<string> userPasswordList = new List<string>();
+            userPasswordList.Add(pass1);
+            userPasswordList.Add(pass1 + "1");
+            userPasswordList.Add(pass1 + "2");
+            UsersDatabase.Add(email, userPasswordList);
+            MessageBox.Show($"Вы зарегистрированы!\nДля вас создано {userPasswordList.Count} одноразовых паролей:\n1) {pass1}\n2) {pass1}1\n3) {pass1}2",
+                            "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            txtBxEmail.Clear();
+            txtBxPassword.Clear();
+            txtBxPassword2.Clear();
         }
     }
 }
